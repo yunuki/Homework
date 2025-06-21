de2f429a75d4d855747fa461dd05b06f47c0c071
yunuki <woogie_ing@naver.com>
2025-06-21 15:41:55 +0900

    Fix: move DI from Shared to Core

--- Homework/Modules/Core/Package.swift
+++ Homework/Modules/Core/Package.swift
@@ -7,17 +7,21 @@ let package = Package(
     name: "Core",
     platforms: [.iOS(.v14)],
     products: [
-        .library(name: "Core", targets: ["Networking", "UIComponents"])
+        .library(name: "Core", targets: ["DI", "Logging", "Networking", "UIComponents"])
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.0")),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "8.0.0"))
+        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "8.0.0")),
+        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.0")
     ],
     targets: [
+        .target(name: "DI",
+                dependencies: [.product(name: "Swinject", package: "Swinject")]),
         .target(name: "Logging"),
         .target(name: "Networking",
                 dependencies: [
                     .product(name: "Alamofire", package: "Alamofire"),
+                    .product(name: "Swinject", package: "Swinject"),
                     .target(name: "Logging")
                 ]),
         .target(name: "UIComponents",
