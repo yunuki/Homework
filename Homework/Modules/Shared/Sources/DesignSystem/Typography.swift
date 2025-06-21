8b1da333d8285a52ddb06e02899ea7f646dfe543
yunuki <woogie_ing@naver.com>
2025-06-21 14:51:02 +0900

    Feat: add Typography

---
+++ Homework/Modules/Shared/Sources/DesignSystem/Typography.swift
@@ -0,0 +1,54 @@
+//
+//  Typography.swift
+//  Shared
+//
+//  Created by 윤재욱 on 6/21/25.
+//
+
+import SwiftUI
+
+public enum Typography: String, CaseIterable {
+    case title
+    case body
+    case caption
+    
+    public var fontSize: CGFloat {
+        switch self {
+        case .title:
+            return 18
+        case .body:
+            return 14
+        case .caption:
+            return 10
+        }
+    }
+    
+    public var fontHeight: CGFloat {
+        return UIFont.systemFont(ofSize: self.fontSize).lineHeight
+    }
+    
+    public var lineHeight: CGFloat {
+        switch self {
+        case .title, .body, .caption:
+            return fontSize * 1.5
+        }
+    }
+}
+
+struct TypographyViewModifier: ViewModifier {
+    let typography: Typography
+    let weight: Font.Weight
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: typography.fontSize, weight: weight))
+            .lineSpacing(typography.lineHeight - typography.fontHeight)
+            .padding(.vertical, (typography.lineHeight - typography.fontHeight) / 2)
+    }
+}
+
+public extension View {
+    func typography(_ typography: Typography, weight: Font.Weight = .regular) -> some View {
+        modifier(TypographyViewModifier(typography: typography, weight: weight))
+    }
+}
